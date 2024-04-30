557c04fc33241e630fd996338167e85bf149c52a
Polina Vlasova <vlpolina@distant.global>
2024-05-01 00:06:24 +0700

    правка фона header

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,47 @@
-import React from 'react';
-import Header from './components/header/header.jsx';
-import About from './components/about/about.jsx';
-import Footer from './components/footer/footer.jsx';
+import React from "react";
+import Header from "./components/header/header.jsx";
+import About from "./components/about/about.jsx";
+import Footer from "./components/footer/footer.jsx";
 // import "./App.scss";
 import "./components/common.scss";
-import Contacts from './components/contacts/contacts.jsx';
-import OurWork from './components/our-work/our-work.jsx';
-import Masters from './components/masters/masters.jsx';
-import Popular from './components/popular/popular.jsx';
-import Services from './components/services/services.jsx';
-import ScrollToTopButton from './components/button-up/button-up.jsx';
+import Contacts from "./components/contacts/contacts.jsx";
+import OurWork from "./components/our-work/our-work.jsx";
+import Masters from "./components/masters/masters.jsx";
+import Popular from "./components/popular/popular.jsx";
+import Services from "./components/services/services.jsx";
+import ScrollToTopButton from "./components/button-up/button-up.jsx";
 // import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 export function App(props) {
   return (
     // <Router>
-      <div className='App'>
-        <link
+    <div>
+      <link
         href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet"/>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link rel="stylesheet" href="https://necolas.github.io/normalize.css/" />
-        <link rel="stylesheet" href="./compiled-styles/styles.css" />
-        <link rel="stylesheet" href="./plugins/hystModal-0.4/dist/hystmodal.min.css"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="description" content="" />
-      
-        <Header /> 
-        <ScrollToTopButton />
-        <About />
-        <Popular />
-        <Services />
-        <Masters />
-        <OurWork />
-        <Contacts />
-        <Footer />
-      </div>
+        rel="stylesheet"
+      />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+      <link rel="stylesheet" href="https://necolas.github.io/normalize.css/" />
+      <link rel="stylesheet" href="./compiled-styles/styles.css" />
+      <link
+        rel="stylesheet"
+        href="./plugins/hystModal-0.4/dist/hystmodal.min.css"
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="description" content="" />
+
+      <Header />
+      <ScrollToTopButton />
+      <About />
+      <Popular />
+      <Services />
+      <Masters />
+      <OurWork />
+      <Contacts />
+      <Footer />
+    </div>
   );
 }
-
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,67 +1,63 @@
-import React from 'react';
+import React from "react";
 import "./header.scss";
 import girl from "../../images/girl.png";
 
+export default function Header() {
+  const scrollTo = (id) => {
+    const element = document.getElementById(id);
+    element.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
 
-export default function Header () {
-    const scrollTo = (id) => {
-        const element = document.getElementById(id);
-        element.scrollIntoView({
-            behavior: 'smooth'
-        });
-    };
-    
-    
-
-
-    return (
-        <header className="header">
-            <div className="content-contaitner content-container__for-header">
-                <div className="header__top">
-                <div className="header__logo">Мир красоты</div>
-                    <nav className="header__nav">
-                        <div>
-                            <ul>
-                                <li onClick={() => scrollTo('about')}>
-                                    <p>О нас</p>
-                                </li>
-                                <li onClick={() => scrollTo('services')}>
-                                    <p>Услуги и цены</p>
-                                </li>
-                                <li onClick={() => scrollTo('masters')}>
-                                    <p>Мастера</p>
-                                </li>
-                                <li onClick={() => scrollTo('our-work')}>
-                                    <p>Наши работы</p>
-                                </li>
-                                <li onClick={() => scrollTo('contacts')}>
-                                    <p>Контакты</p>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
-                    <div className="header__info">
-                        <p className="header__phone-number">8 (499) 686-17-62</p>
-                        <p className="header__schedule">с 10.00 до 19.00 без выходных</p>
-                    </div>
-                    
-                </div>
-                <div className="header__content">
-                    <div className="header__hero">
-                        <div className="header__short-description">
-                            <h2>Создай свой образ вместе с нами</h2>
-                            <p>Идеальный образ за 60 минут</p>
-                        </div>
-                        <button className="button button__big" data-hystmodal="#submit-modal">
-                            Записаться онлайн
-                        </button>
-                    </div>
-                        <div className="header__social"></div>
-                </div>
-                <img className="header__girl" src={girl} alt="girl" />
-                {/* <img className="background" src={background} alt="background" /> */}
+  return (
+    <header>
+      <div className="content-container__for-header">
+        <div className="header__top">
+          <div className="header__logo">Мир красоты</div>
+          <nav className="header__nav">
+            <div>
+              <ul>
+                <li onClick={() => scrollTo("about")}>
+                  <p>О нас</p>
+                </li>
+                <li onClick={() => scrollTo("services")}>
+                  <p>Услуги и цены</p>
+                </li>
+                <li onClick={() => scrollTo("masters")}>
+                  <p>Мастера</p>
+                </li>
+                <li onClick={() => scrollTo("our-work")}>
+                  <p>Наши работы</p>
+                </li>
+                <li onClick={() => scrollTo("contacts")}>
+                  <p>Контакты</p>
+                </li>
+              </ul>
             </div>
-        </header>
-    )
+          </nav>
+          <div className="header__info">
+            <p className="header__phone-number">8 (499) 686-17-62</p>
+            <p className="header__schedule">с 10.00 до 19.00 без выходных</p>
+          </div>
+        </div>
+        <div className="header__content">
+          <div className="header__hero">
+            <div className="header__short-description">
+              <h2>Создай свой образ вместе с нами</h2>
+              <p>Идеальный образ за 60 минут</p>
+            </div>
+            <button
+              className="button button__big"
+              data-hystmodal="#submit-modal">
+              Записаться онлайн
+            </button>
+          </div>
+          <div className="header__social"></div>
+        </div>
+        <img className="header__girl" src={girl} alt="girl" />
+        {/* <img className="background" src={background} alt="background" /> */}
+      </div>
+    </header>
+  );
 }
-
